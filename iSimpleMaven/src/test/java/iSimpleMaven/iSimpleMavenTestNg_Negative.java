8010813aefdd86f3cdfadc3faa787dbbb93812c3
SathishMurugiah <SathishMurugiah@DESKTOP-5L9D1ND.T-mobile.com>
2018-05-06 18:27:59 -0400

    First Commit to First Git Repo

---
+++ iSimpleMaven/src/test/java/iSimpleMaven/iSimpleMavenTestNg_Negative.java
@@ -0,0 +1,47 @@
+package iSimpleMaven;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class iSimpleMavenTestNg_Negative {
+
+	
+	@Test (priority = 2)
+	public void SampleTestScript1 () {
+		System.out.println("Test Case Number 1");
+	}
+
+	@Test
+	public void SampleTestScript2 () {
+		System.out.println("Test Case Number 2");
+		Assert.assertEquals("Google", "Hello");
+	}
+	
+	@Test (priority = 3)
+	public void SampleTestScript3 () {
+		System.out.println("Test Case Number 3");
+	}
+	
+	@Test
+	public void SampleTestScript4 () {
+		System.out.println("Test Case Number 4");
+	}
+	
+	
+	@Test (priority = 1)
+	public void SampleTestScript5() {
+		System.out.println("Test Case Number 5");
+	}
+	
+	@Test
+	public void SampleTestScript6() {
+		System.out.println("Test Case Number 6");
+	}
+	
+	@Test (priority = 0)
+	public void SampleTestScript7() {
+		System.out.println("Test Case Number 7");
+	}
+	
+	
+}
